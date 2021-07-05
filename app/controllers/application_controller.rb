19bcb539acf17773f81e8d05461181923de3540a
Tobias Schmid <2toschmid@googlemail.com>
2021-07-05 19:16:56 +0200

    error handling, bug fixing, production dockerization fixes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,12 +5,28 @@ class ApplicationController < ActionController::API
   ActionController::HttpAuthentication::Token.const_set("TOKEN_REGEX", /^(Token|Bearer|System|token|bearer|system)\s+/)
   before_action :authenticate
 
+  # overwrite all exception to be sure we catch it
+  rescue_from Exception, :with => :error_generic
+
+  rescue_from Faraday::BadRequestError, :with => :error_cwa_server
+  rescue_from ArgumentError, :with => :error_generic
+  rescue_from RuntimeError, :with => :error_generic
+  rescue_from NoMethodError, :with => :error_generic
+
   private
     def authenticate
       authenticate_or_request_with_http_token do |token, options|
         # Compare the tokens in a time-constant manner, to mitigate
         # timing attacks.
-        ActiveSupport::SecurityUtils.secure_compare(token, Rails.application.credentials.system[:token])
+        ActiveSupport::SecurityUtils.secure_compare(token, ENV["SERVER_TOKEN"]) # /*Rails.application.credentials.system[:token]*/
       end
+    end
+    def error_generic(error)
+      logger.error error
+      render json: { errors: [error.to_s] }, status: error.respond_to?(:status) ? error.status : 500
+    end
+    def error_cwa_server(error)
+      logger.error error
+      render json: { errors: [error.response[:body]] }, status: error.response[:status]
     end
 end
--- app/controllers/test_result_controller.rb
+++ app/controllers/test_result_controller.rb
@@ -8,9 +8,17 @@ class TestResultController < ApplicationController
 
     # result must be between 5=pending, 6=negative, 7=positive, 8=test invalid
     unless params[:result] >= 5 && params[:result] <= 8
-      raise "testresult for the given test must be between: 5=pending, 6=negative, 7=positive, 8=test invalid"
+      raise ArgumentError.new("testresult for the given test must be between: 5=pending, 6=negative, 7=positive, 8=test invalid")
     end
-    params[:test_result]
+
+    test_result = params[:test_result]
+    # take timestamp as utc timestamp or string representation
+    logger.info "parse given test-timestamp...#{params}"
+    test_result[:timestamp] = test_result[:timestamp].is_a?(String) ? Time.parse(test_result[:timestamp]).to_i : Time.at(test_result[:timestamp]).to_i
+    logger.info "parse given birthdate..."
+    test_result[:dob] = Date.parse(test_result[:dob]).strftime("%Y-%m-%d")
+
+    test_result
   end
 
   # cwa_test_id also named SHA256-Hash or hash
@@ -26,16 +34,20 @@ class TestResultController < ApplicationController
   end
 
   def CWA_request test_results
-    # production: https://quicktest-result.coronawarn.app
-    # testing: https://quicktest-result-dfe4f5c711db.coronawarn.app/
-    cwa_url = Rails.env.production? ? "https://quicktest-result.coronawarn.app/" : "https://quicktest-result-dfe4f5c711db.coronawarn.app/" #"https://quicktest-result-cff4f7147260.coronawarn.app/"
+    #"https://quicktest-result-cff4f7147260.coronawarn.app/"
+    # cwa_url = Rails.env.production? ? "https://quicktest-result.coronawarn.app/" : "https://quicktest-result-dfe4f5c711db.coronawarn.app/"
+    cwa_url = ENV["CWA_URL"]
     logger.info "push to cwa server #{cwa_url}..."
-    # byebug
+
+    client_key = OpenSSL::PKey::RSA.new(File.read('config/credentials/covidgo-wru.key'), ENV["KEY_PASSWORD"])
+    client_cert = OpenSSL::X509::Certificate.new(File.read('config/credentials/covidgo-wru.schnelltestportal.de-Server-17f5abbefa6fb59dfa43f1dc8bc4ddfd.cer'))
     cwa_server_connection = Faraday.new cwa_url,
       ssl: {
-        client_key: OpenSSL::PKey::RSA.new(File.read('config/credentials/covidgo-wru.key'), Rails.application.credentials.cwa[:key_password]),
-        client_cert: OpenSSL::X509::Certificate.new(File.read('config/credentials/covidgo-wru.schnelltestportal.de-Server-17f5abbefa6fb59dfa43f1dc8bc4ddfd.cer'))
-      }
+        client_key: client_key,
+        client_cert: client_cert
+      } do |c|
+        c.use Faraday::Response::RaiseError
+      end
 
     cwa_server_response = cwa_server_connection.post("api/v1/quicktest/results") do |request|
       request.headers['Content-Type'] = 'application/json'
@@ -49,13 +61,6 @@ class TestResultController < ApplicationController
   def create
     test_result = test_result_params
     # qr_code.testid = SecureRandom.uuid unless qr_code.testid.present? # with statless service we need testid from extern
-
-    # take timestamp as utc timestamp or string representation
-    logger.info "parse given test-timestamp...#{params}"
-    test_result[:timestamp] = test_result[:timestamp].is_a?(String) ? Time.parse(test_result[:timestamp]).to_i : Time.at(test_result[:timestamp]).to_i
-    logger.info "parse given birthdate..."
-    test_result[:dob] = Date.parse(test_result[:dob]).strftime("%Y-%m-%d")
-
     # generate 128-bit salt
     logger.info "create secure salt..."
     test_result[:salt] = SecureRandom.hex(16) # "759F8FF3554F0E1BBF6EFF8DE298D9E9" # SecureRandom.hex(16)
@@ -86,11 +91,8 @@ class TestResultController < ApplicationController
     logger.info "push to cwa server..."
     cwa_server_response = CWA_request test_results
     logger.info "response: #{cwa_server_response.body}"
-    byebug
-    if cwa_server_response.success?
-      render json: test_result.to_json
-    else
-      render json: { error: cwa_server_response.body}, status: cwa_server_response.status
-    end
+
+    # cwa server returns 204 - no content if it succeeds, as we want to return the data we transform it to 200 - OK
+    render json: test_result.to_json, status: cwa_server_response.success? ? :ok : cwa_server_response.status
   end
 end
